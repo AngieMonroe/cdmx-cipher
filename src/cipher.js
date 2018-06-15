ebe20a2e24ca0adf3689903e65fa01a10e2acbef
AngieMonroe <ame.monroy@gmail.com>
2018-06-15 14:39:22 -0500

    Versión cuasi-final, ya pasa nuevos tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,8 @@
 window.cipher = {
  encode : (offset,string) => {
-   //llamo el primer parametro de la función desde un input
-   string = document.getElementById ("textoIngresado").value;
-   console.log (string)
-   //llamo segundo parametro de la función mediante un input
-   offset = parseInt(document.getElementById ("clave").value);
-   console.log (offset);
    //creo variables que me ayudarán a almacenar información durante el
    //recorrido del for
    let valorAscii = 0;
-   //comentario
-   let resultPosition = 0;
-   let resultOffset = 0;
-   let resultResiduo = 0;
    let positionNew = 0;
    let cambiandoAletra = "";
    let cifrado = "";
@@ -25,45 +15,31 @@ window.cipher = {
      //Creo condicionales para que de acuerdo al valor de ascii se realice la operación
      //correspondiente al caracter y así identificar si es mayúsculas o minúsculas
      if (valorAscii >= 65 && valorAscii <= 90) {
-       resultPosition = valorAscii - 65;
-       resultOffset = resultPosition + offset;
-       resultResiduo = resultOffset % 26;
-       positionNew = resultResiduo + 65;
-       /*positionNew = valorAscii - 65 + offset % 26 + 65
-         console.log (positionNew);*/
+       positionNew = ((((valorAscii - 65) + offset) % 26) + 65);
+      console.log (positionNew);
      }
      else
      if (valorAscii >= 97 && valorAscii <= 122) {
-       resultPosition = valorAscii - 97;
-       resultOffset = resultPosition + offset;
-       resultResiduo = resultOffset % 26;
-       positionNew = resultResiduo + 97;
-       /*positionNew = valorAscii - 97  + offset % 26) + 97;
-         console.log (positionNew);*/
+       positionNew = ((((valorAscii - 97 ) + offset) % 26) + 97);
+         console.log (positionNew);
      }
      else {
      console.log (valorAscii);
       positionNew = valorAscii;
      }
-     console.log (positionNew);
      cambiandoALetra = String.fromCharCode(positionNew);
      cifrado += cambiandoALetra;
+     console.log(cifrado);
      }
-     document.getElementById("resultadoMensaje").innerHTML= cifrado;
+     //document.getElementById("resultadoMensaje").innerHTML= cifrado;
+     return cifrado;
  },
 
+//////////////////
+
  decode : (offset,string) => {
-   //llamo el primer parametro de la función desde un input
-   string = document.getElementById ("textoIngresado").value;
-   console.log (string)
-   //llamo segundo parametro de la función mediante un input
-   offset = parseInt(document.getElementById ("clave").value);
-   console.log (offset);
    //creo una variable vacia que sera el resultado del recorrido que se hará
    let valorAscii = 0;
-   let resultPosition = 0;
-   let resultOffset = 0;
-   let resultResiduo = 0;
    let positionNew = 0;
    let cambiandoAletra = "";
    let descifrar = "";
@@ -78,21 +54,13 @@ window.cipher = {
      //Creo condicionales para que de acuerdo al valor de ascii se realice la operación
      //correspondiente al caracter
      if (valorAscii >= 65 && valorAscii <= 90) {
-       resultPosition = valorAscii + 65;
-       resultOffset = resultPosition - offset;
-       resultResiduo = resultOffset % 26;
-       positionNew = resultResiduo + 65;
-       /*positionNew = valorAscii - 65 + offset % 26 + 65
-         console.log (positionNew);*/
-     }
+       positionNew = ((((valorAscii + 65 ) - offset) % 26) + 65);
+         console.log (positionNew);
+      }
      else
      if (valorAscii >= 97 && valorAscii <= 122) {
-       resultPosition = valorAscii - 122;
-       resultOffset = resultPosition - offset;
-       resultResiduo = resultOffset % 26;
-       positionNew = resultResiduo + 122;
-       /*positionNew = valorAscii - 97  + offset % 26) + 97;
-         console.log (positionNew);*/
+       positionNew = ((((valorAscii - 122) - offset) % 26) + 122);
+         console.log (positionNew);
      }
      else {
      console.log (valorAscii);
@@ -102,6 +70,7 @@ window.cipher = {
      cambiandoALetra = String.fromCharCode(positionNew);
      descifrar += cambiandoALetra;
      }
-     document.getElementById("resultadoMensaje").innerHTML= descifrar;
+     //document.getElementById("resultadoMensaje").innerHTML= descifrar;
+     return descifrar;
  }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-//import { encode } from "punycode";
+let str1 = document.getElementById ("textoIngresado");
+let str2 = document.getElementById ("textoIngresado");
+let clave = document.getElementById ("clave");
+//crear una función para cifrar el Mensaje
+const cifrar1= () => {
+  let string = str1.value;
+  let offset = parseInt(clave.value);
+
+ document.getElementById("resultadoMensaje").innerHTML = cipher.encode(offset,string);
+
+};
+
+//Creando función para descifrado
+const descifrar1 = () => {
+  let string = str2.value;
+  let offset = parseInt(clave.value);
+
+document.getElementById("resultadoMensaje").innerHTML = cipher.decode(offset,string);
+
+};
